bf634c91c7a4dd0a8c1604209ee841ca064e5249
eordiyants <e.ordiyants@ctrl2go.solutions>
2025-09-29 15:34:43 +0300

    Обновлённый запуск приложения + добавление запуска миграций

--- cmd/exocalc/main.go
+++ cmd/exocalc/main.go
@@ -1,12 +1,34 @@
 package main
 
 import (
-	"develop-internet-applications/internal/api"
-	"log"
+	"develop-internet-applications/internal/app/config"
+	"develop-internet-applications/internal/app/dsn"
+	"develop-internet-applications/internal/app/handler"
+	"develop-internet-applications/internal/app/repository"
+	"develop-internet-applications/internal/pkg"
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+	"github.com/sirupsen/logrus"
 )
 
 func main() {
-	log.Println("Application start!")
-	api.StartServer()
-	log.Println("Application terminated!")
+	router := gin.Default()
+	conf, err := config.NewConfig()
+	if err != nil {
+		logrus.Fatalf("error loading config: %v", err)
+	}
+
+	postgresString := dsn.FromEnv()
+	fmt.Println(postgresString)
+
+	rep, errRep := repository.New(postgresString)
+	if errRep != nil {
+		logrus.Fatalf("error initializing repository: %v", errRep)
+	}
+
+	hand := handler.NewHandler(rep)
+
+	application := pkg.NewApp(conf, router, hand)
+	application.RunApp()
 }
