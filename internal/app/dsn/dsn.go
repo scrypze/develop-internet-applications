8d8e06ac2e987577eb9f55916c019e6d6e77a3ae
eordiyants <e.ordiyants@ctrl2go.solutions>
2025-09-30 18:22:16 +0300

    updated dsn

--- internal/app/dsn/dsn.go
+++ internal/app/dsn/dsn.go
@@ -14,6 +14,6 @@ func FromEnv() string {
 	user := os.Getenv("DB_USER")
 	pass := os.Getenv("DB_PASS")
 	dbname := os.Getenv("DB_NAME")
-    // И вот мы возвращаем dsn, который необходим для подключения к БД
+
 	return fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", host, port, user, pass, dbname)
 }
