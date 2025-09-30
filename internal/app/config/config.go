e5a7dbfdaeaed5bdb793aa80f0ae6f63bebe269c
eordiyants <e.ordiyants@ctrl2go.solutions>
2025-09-30 18:19:45 +0300

    updated config

--- internal/app/config/config.go
+++ internal/app/config/config.go
@@ -2,8 +2,8 @@ package config
 
 import (
 	"os"
-	
-    "github.com/joho/godotenv"
+
+	"github.com/joho/godotenv"
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
 )
@@ -16,25 +16,25 @@ type Config struct {
 func NewConfig() (*Config, error) {
 	var err error
 
-   configName := "config"
-   _ = godotenv.Load()
-   if os.Getenv("CONFIG_NAME") != "" {
-      configName = os.Getenv("CONFIG_NAME")
-   }
+	configName := "config"
+	_ = godotenv.Load()
+	if os.Getenv("CONFIG_NAME") != "" {
+		configName = os.Getenv("CONFIG_NAME")
+	}
 
-   viper.SetConfigName(configName)
-   viper.SetConfigType("toml")
-   viper.AddConfigPath("config")
-   viper.AddConfigPath(".")
-   viper.WatchConfig()
+	viper.SetConfigName(configName)
+	viper.SetConfigType("toml")
+	viper.AddConfigPath("config")
+	viper.AddConfigPath(".")
+	viper.WatchConfig()
 
 	err = viper.ReadInConfig()
 	if err != nil {
 		return nil, err
 	}
 
-	cfg := &Config{} // создаем объект конфига
-	err = viper.Unmarshal(cfg) // читаем информацию из файла, 
+	cfg := &Config{}           // создаем объект конфига
+	err = viper.Unmarshal(cfg) // читаем информацию из файла,
 	// конвертируем и затем кладем в нашу переменную cfg
 	if err != nil {
 		return nil, err
