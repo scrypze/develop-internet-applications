72e5961239df7c05a099342af406d2b12bbf8258
eordiyants <e.ordiyants@ctrl2go.solutions>
2025-10-01 17:46:15 +0300

    zxc

--- internal/app/ds/CalculateExoplanets.go
+++ internal/app/ds/CalculateExoplanets.go
@@ -1,8 +1,8 @@
 package ds
 
 type CalculateExoplanets struct {
-	SelectionStarsID int    `gorm:"primaryKey;column:selection_stars_id"` // FK -> selected_stars.id
-	StarID           int    `gorm:"primaryKey;column:star_id"`            // FK -> stars.id
+	SelectionStarsID int    `gorm:"primaryKey;column:selection_stars_id"` 
+	StarID           int    `gorm:"primaryKey;column:star_id"`            
 	Comment          string `gorm:"type:text"`
 }
 
