1b58a33882d52797ae0f2ae70ca176db62415cca
eordiyants <e.ordiyants@ctrl2go.solutions>
2025-10-03 23:47:30 +0300

    seeds.sql

--- Makefile
+++ Makefile
@@ -7,6 +7,5 @@ migrate:
 	go run cmd/migrate/main.go
 
 PHONY: insert
-insert-stars:
-	docker compose exec -T postgres psql -U myuser -d exocalc -v ON_ERROR_STOP=1 -f - < '/Users/erikray/Developer/develop-internet-applications/insert_stars.sql'
-	docker compose exec -T postgres psql -U myuser -d exocalc -v ON_ERROR_STOP=1 -f - < '/Users/erikray/Developer/develop-internet-applications/insert.sql'
+insert:
+	docker compose exec -T postgres psql -U myuser -d exocalc -v ON_ERROR_STOP=1 -f - < '/Users/erikray/Developer/develop-internet-applications/seeds.sql'
---
+++ seeds.sql
@@ -0,0 +1,31 @@
+BEGIN;
+
+INSERT INTO users (login, password, is_moderator)
+VALUES ('creator', 'x', false)
+ON CONFLICT (login) DO NOTHING;
+
+INSERT INTO stars (
+  id, title, description, image_path, spectral_type, temperature,
+  radius, mass, luminosity, metallicity, age, distance
+) VALUES
+  (1,'Gliese 667','Красный карлик спектрального класса M1.5V','http://localhost:9000/exocalc/gliese-667.png','M1.5V','~3700 K','~0.42 R☉','~0.33 M☉','~0.014 L☉','-0.55','~2-10 млрд лет','~6.8 pc (~22 световых лет)'),
+  (2,'TRAPPIST-1','Ультрахолодный красный карлик спектрального класса M8V','http://localhost:9000/exocalc/trappist-1.png','M8V','~2550 K','~0.12 R☉','~0.09 M☉','~0.0005 L☉','0.04','~7.6 млрд лет','~12.1 pc (~39 световых лет)'),
+  (3,'Ross 128','Красный карлик спектрального класса M4V','http://localhost:9000/exocalc/ross-128.png','M4V','~3192 K','~0.21 R☉','~0.16 M☉','~0.0036 L☉','0.00','~9.45 млрд лет','~3.37 pc (~11 световых лет)'),
+  (4,'Proxima Centauri','Красный карлик спектрального класса M5.5V','http://localhost:9000/exocalc/proxima.png','M5.5V','~3042 K','~0.15 R☉','~0.12 M☉','~0.0017 L☉','0.21','~4.85 млрд лет','~1.30 pc (~4.24 световых лет)')
+ON CONFLICT (id) DO NOTHING;
+
+WITH creator AS (SELECT id FROM users WHERE login='creator' LIMIT 1)
+INSERT INTO selected_stars (id, status, scientist, creator_id, date)
+SELECT 1, 'draft', 'Михаил Ломоносов', creator.id, DATE '2003-10-12'
+FROM creator
+ON CONFLICT (id) DO NOTHING;
+
+INSERT INTO calculate_exoplanets (
+  selection_stars_id, star_id, comment, probable_number_of_planets, habitable_zone
+) VALUES
+  (1,1,'',2.5,'0.12-0.24 a.e.'),
+  (1,2,'',2.5,'0.12-0.24 a.e.'),
+  (1,3,'',2.5,'0.12-0.24 a.e.')
+ON CONFLICT DO NOTHING;
+
+COMMIT;
