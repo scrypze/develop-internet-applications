6fbe47411b13e4d0db654dd46a45f13f68b09540
eordiyants <e.ordiyants@ctrl2go.solutions>
2025-10-03 23:22:23 +0300

    правка таблиц

--- internal/app/ds/CalculateExoplanets.go
+++ internal/app/ds/CalculateExoplanets.go
@@ -1,12 +1,13 @@
 package ds
 
 type CalculateExoplanets struct {
-	SelectionStarsID        int     `gorm:"primaryKey;column:selection_stars_id"`
-	StarID                  int     `gorm:"primaryKey;column:star_id"`
+	SelectionStarsID int    `gorm:"not null;uniqueIndex:idx_sel_star"`
+	StarID           int    `gorm:"not null;uniqueIndex:idx_sel_star"`
 	Comment                 string  `gorm:"type:text"`
+	
 	ProbableNumberOfPlanets float32 `gorm:"type:real"`
 	HabitableZone           string  `gorm:"type:varchar(64)"`
 
-	SelectedStars SelectedStars `gorm:"foreignKey:SelectionStarsID;references:ID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
-	Star          Star          `gorm:"foreignKey:StarID;references:ID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
+	SelectedStars SelectedStars `gorm:"foreignKey:SelectionStarsID"`
+	Star          Star          `gorm:"foreignKey:StarID"`
 }
