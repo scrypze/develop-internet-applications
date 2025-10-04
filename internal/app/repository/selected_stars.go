aba0995bfe7375a1d461213f7ae06b6c48c1577f
eordiyants <e.ordiyants@ctrl2go.solutions>
2025-10-04 22:37:49 +0300

    zxc

--- internal/app/repository/selected_stars.go
+++ internal/app/repository/selected_stars.go
@@ -3,6 +3,8 @@ package repository
 import (
 	"develop-internet-applications/internal/app/model"
 	"fmt"
+
+	"github.com/sirupsen/logrus"
 )
 
 func (r *Repository) GetSelectedStars() ([]model.SelectedStars, error) {
@@ -22,4 +24,32 @@ func (r *Repository) GetSelectedStarsByID(id int) (model.SelectedStars, error) {
 		return model.SelectedStars{}, err
 	}
 	return list, nil
+}
+
+func (r *Repository) GetSelectedStarsCount() int64 {
+	var selectedStarsID int
+	var count int64
+
+	creatorID := 1
+
+	
+	err := r.db.Model(&model.SelectedStars{}).
+		Where("creator_id = ? AND status = ?", creatorID, "draft").
+		Select("id").
+		First(&selectedStarsID).Error
+
+	if err != nil {
+		return 0
+	}
+
+	err = r.db.Model(&model.CalculateExoplanets{}).
+		Where("selected_stars_id = ?", selectedStarsID).
+		Count(&count).Error
+
+	if err != nil {
+		logrus.Error("error counting records in calculate_exoplanets:", err)
+		return 0
+	}
+
+	return count
 }
