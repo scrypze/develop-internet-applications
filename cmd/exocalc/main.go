463c27d321aa0b6e62b0fa71d9d860c008b1ef6a
eordiyants <e.ordiyants@ctrl2go.solutions>
2025-10-06 23:56:47 +0300

    Более чистая архитектура

--- cmd/exocalc/main.go
+++ cmd/exocalc/main.go
@@ -1,34 +1,9 @@
 package main
 
 import (
-	"develop-internet-applications/internal/app/config"
-	"develop-internet-applications/internal/app/dsn"
-	"develop-internet-applications/internal/app/handler"
-	"develop-internet-applications/internal/app/repository"
 	"develop-internet-applications/internal/pkg"
-	"fmt"
-
-	"github.com/gin-gonic/gin"
-	"github.com/sirupsen/logrus"
 )
 
 func main() {
-	router := gin.Default()
-	conf, err := config.NewConfig()
-	if err != nil {
-		logrus.Fatalf("error loading config: %v", err)
-	}
-
-	postgresString := dsn.FromEnv()
-	fmt.Println(postgresString)
-
-	rep, errRep := repository.New(postgresString)
-	if errRep != nil {
-		logrus.Fatalf("error initializing repository: %v", errRep)
-	}
-
-	hand := handler.NewHandler(rep)
-
-	application := pkg.NewApp(conf, router, hand)
-	application.RunApp()
+	pkg.RunApp()
 }
--- internal/pkg/app.go
+++ internal/pkg/app.go
@@ -1,10 +1,19 @@
 package pkg
 
 import (
+	"context"
 	"fmt"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"develop-internet-applications/internal/app/config"
+	"develop-internet-applications/internal/app/dsn"
 	"develop-internet-applications/internal/app/handler"
+	"develop-internet-applications/internal/app/repository"
+
 	"github.com/gin-gonic/gin"
 	"github.com/sirupsen/logrus"
 )
@@ -30,8 +39,45 @@ func (a *Application) RunApp() {
 	a.Handler.RegisterStatic(a.Router)
 
 	serverAddress := fmt.Sprintf("%s:%d", a.Config.ServiceHost, a.Config.ServicePort)
-	if err := a.Router.Run(serverAddress); err != nil {
-		logrus.Fatal(err)
+	srv := &http.Server{Addr: serverAddress, Handler: a.Router}
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			logrus.Fatalf("listen: %v", err)
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	logrus.Info("Server shutting down")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		logrus.Fatalf("Server forced to shutdown: %v", err)
 	}
 	logrus.Info("Server down")
+}
+
+func RunApp() {
+	router := gin.Default()
+
+	conf, err := config.NewConfig()
+	if err != nil {
+		logrus.Fatalf("error loading config: %v", err)
+	}
+
+	postgresString := dsn.FromEnv()
+	fmt.Println(postgresString)
+
+	rep, errRep := repository.New(postgresString)
+	if errRep != nil {
+		logrus.Fatalf("error initializing repository: %v", errRep)
+	}
+
+	hand := handler.NewHandler(rep)
+
+	application := NewApp(conf, router, hand)
+	application.RunApp()
 }
